cb27f8f615b860d2e2c60d511185c7b04eb71e62
AyoAlfonso <alfonsoayo7@gmail.com>
2020-01-08 09:01:52 +0100

    WWW Redirect

--- src/server.js
+++ src/server.js
@@ -20,25 +20,25 @@ express()
 	.use(
 		compression({ threshold: 0 }),
 	
-		// async function (req, res, next) {
-		// 	let www = 1
-		// 	if ('production') {
-		// 		var host = req.header('host');
-		// 		if (www) {
-		// 			var correctHost = host.match(/^www\..*/i);
+		async function (req, res, next) {
+			let www = 1
+			if ('production') {
+				var host = req.header('host');
+				if (www) {
+					var correctHost = host.match(/^www\..*/i);
 
-		// 			if (!correctHost) {
-		// 				return res.redirect(301, 'https://www.' + host);
-		// 			}
-		// 		}
-		// 		if (req.headers['x-forwarded-proto'] !== 'https') {
-		// 			return res.redirect(statusCode, 'https://' + req.hostname + req.originalUrl);
-		// 		}
-		// 		next();
-		// 	} else {
-		// 		next();
-		// 	}
-		// },
+					if (!correctHost) {
+						return res.redirect(301, 'https://www.' + host);
+					}
+				}
+				if (req.headers['x-forwarded-proto'] !== 'https') {
+					return res.redirect(statusCode, 'https://' + req.hostname + req.originalUrl);
+				}
+				next();
+			} else {
+				next();
+			}
+		},
 		
 		sirv('static', { dev }),
 		apiProxy,
