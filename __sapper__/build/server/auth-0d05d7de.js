216ab006a720446ca1a1f9883d5580ead0c54435
AyoAlfonso <alfonsoayo7@gmail.com>
2020-01-09 13:43:07 +0100

    Add backend API url

--- __sapper__/build/server/auth-0d05d7de.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
-var Cookie = _interopDefault(require('cookie-universal'));
-require('./api-4b695258.js');
-require('./app-7e6d47ce.js');
-
-const cookies = Cookie();
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const LOCAL_STORE_URL = 'http://localhost:4400'
 const REMOTE_STORE_URL = 'https://www.hopyshopy.com'
 const apiUrl = 'https://api.litekart.in'
-const nodeApiUrl = (process.env.NODE_ENV === 'production') ? 'http://ampzbackend-env.5mtwmzvd3b.us-east-2.elasticbeanstalk.com' : 'http://localhost:3000'
+const nodeApiUrl = (process.env.NODE_ENV === 'production') ? 'https://ampz-backend-secured.us-east-2.elasticbeanstalk.com' : 'http://localhost:3000'
 const storeUrl = (process.env.NODE_ENV === 'production') ? REMOTE_STORE_URL : LOCAL_STORE_URL
 export const typingTimeout = 0 // After this delay the search api will be fired
 const HOST = 'https://www.hopyshopy.com'
