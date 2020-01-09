d0274635837cf73b20ccf6b67e546c328c2e3c38
AyoAlfonso <alfonsoayo7@gmail.com>
2020-01-09 12:38:02 +0100

    Added build file

--- __sapper__/build/server/auth-74e0780c.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
-var Cookie = _interopDefault(require('cookie-universal'));
-require('./api-d6db2657.js');
-require('./app-605a28d4.js');
-
-const cookies = Cookie();
