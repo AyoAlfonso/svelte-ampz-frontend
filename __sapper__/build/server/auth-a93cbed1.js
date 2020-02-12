6fce28bdbc3a3ebc27b3ca0c2c4768b8109176e5
AyoAlfonso <alfonsoayo7@gmail.com>
2020-02-12 13:29:21 +0100

    Add new ampz api url

---
+++ __sapper__/build/server/auth-a93cbed1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var Cookie = _interopDefault(require('cookie-universal'));
+require('./api-8725a28b.js');
+require('./app-df97e3a1.js');
+
+const cookies = Cookie();
