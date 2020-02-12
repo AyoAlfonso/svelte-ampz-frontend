77d71fe22469404d3992775494d2a4ec752b99ae
AyoAlfonso <alfonsoayo7@gmail.com>
2020-02-12 13:11:32 +0100

    Add new ampz api url

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 const LOCAL_STORE_URL = 'http://localhost:4400'
 const REMOTE_STORE_URL = 'https://www.hopyshopy.com'
 const apiUrl = 'https://api.litekart.in'
-const nodeApiUrl = (process.env.NODE_ENV === 'production') ? 'https://ampz-backend-secured.us-east-2.elasticbeanstalk.com' : 'http://localhost:3000'
+const nodeApiUrl = (process.env.NODE_ENV === 'production') ? 'https://api.ampz.tv' : 'http://localhost:3000'
 const storeUrl = (process.env.NODE_ENV === 'production') ? REMOTE_STORE_URL : LOCAL_STORE_URL
 export const typingTimeout = 0 // After this delay the search api will be fired
 const HOST = 'https://www.hopyshopy.com'
 const currency = { symbol: '₹', code: 'INR' }
 const tokenExpiry = 60 * 60 * 24 * 365 // Login validity in seconds
-const ANALYTICS_TRACKING_ID = 'UA-49421899-5'
+const ANALYTICS_TRACKING_ID = 'UA-4942e1899-5'
 const sorts = [
     { name: "Relevance", val: null },
     { name: "Whats New", val: "-createdAt" },
